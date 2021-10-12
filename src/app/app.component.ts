ceaabdd8acd60a6de4d97f4351bc02fe944c0cd7
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-10-12 14:26:13 +0200

    Finished Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../../node_modules/bootstrap/dist/css/bootstrap.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type: 'server', name: 'Test server', content: 'Just a test!'}];
+
+  onServerAdded(serverData: {serverName:string, serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+
+
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, ContentChild, ElementRef,
+  Input,
+  OnChanges,
+  OnInit, ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
-export class ServerElementComponent implements OnInit {
-  element: {type: string, name:string, content: string} | undefined;
+export class ServerElementComponent implements OnInit, OnChanges {
+  // @ts-ignore
+  @Input('srvElement') element: {type: string, name:string, content: string};
+  // @ts-ignore
+  @ViewChild('heading') header: ElementRef
+  // @ts-ignore
+  @ContentChild('contentParagraph') paragraph: ElementRef
 
-  constructor() { }
+  constructor() {
+  }
+
+  ngOnChanges(){
+
+  }
 
   ngOnInit(): void {
   }
